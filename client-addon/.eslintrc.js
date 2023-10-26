d50a896155bd8d064877f2b2be5d88e8ee51986b
Robin <robin.bauhn@minso.se>
2023-10-26 12:25:27 +0200

    fix: bumped dependencies

--- client-addon/.eslintrc.js
+++ client-addon/.eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ['error', 'always-multiline'],
+    "no-console": process.env.NODE_ENV === "production" ? 1 : 0,
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "comma-dangle": [1, "always-multiline"]
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint"
   },
   globals: {
-    'ClientAddonApi': false,
-    'mapSharedData': false,
-    'Vue': false,
-  },
-}
+    ClientAddonApi: false,
+    mapSharedData: false,
+    Vue: false
+  }
+};
